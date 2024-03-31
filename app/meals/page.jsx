a734b26b9813f21abb787c791a08d00b34d89539
Prashant kumhar <prkumar@griddynamics.com>
2024-03-31 15:13:10 +0530

    fetching data from backend and loading is done

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,23 +1,29 @@
 import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
 import Link from 'next/link';
 import classes from './page.module.css'
 
-const MealsPage = () => {
+const MealsPage = async () => {
+  const meals = await getMeals();
   return (
     <>
       <header className={classes.header}>
         <h1>
-          Delicious meals, created {' '}
+          Delicious meals, created{" "}
           <span className={classes.highlight}>by you</span>
         </h1>
-        <p>Choose your favorite recipe and cook it yourself. It is easy and fun</p>
-        <p className={classes.cta}><Link href="/meals/share">Share Your Favorite Recipe</Link></p>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]}/>
+        <MealsGrid meals={meals} />
       </main>
     </>
-  )
-}
+  );
+};
 
 export default MealsPage;
