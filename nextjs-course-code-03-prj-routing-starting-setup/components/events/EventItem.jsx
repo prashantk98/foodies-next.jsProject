2c464c90068bffed7d0dc064c711e7ffdc106490
Prashant kumhar <prkumar@griddynamics.com>
2024-04-04 00:34:15 +0530

    icons are added

--- nextjs-course-code-03-prj-routing-starting-setup/components/events/EventItem.jsx
+++ nextjs-course-code-03-prj-routing-starting-setup/components/events/EventItem.jsx
@@ -1,5 +1,8 @@
-import Link from 'next/link';
-import classes from './event-item.module.css'
+import Button from '../ui/button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
+import classes from './event-item.module.css';
 const EventItem = (props) => {
   const { title, image, date, location, id } = props;
   const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
@@ -14,17 +17,26 @@ const EventItem = (props) => {
       <img src={"/" + image} alt={title} />
       <div className={classes.content}>
         <div className={classes.summary}>
-
-        <h2>{title}</h2>
-        <div className={classes.date} >
-          <time>{humanReadableDate}</time>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <DateIcon/>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <AddressIcon/>
+            <address>{formateLocation}</address>
+          </div>
         </div>
-        <div className={classes.address} >
-          <address>{formateLocation}</address>
-        </div>
-        </div>
-        <div className={classes.actions} >
-          <Link href={exploreLink}>Explore Event</Link>
+        <div className={classes.actions}>
+          {/* <Link href={exploreLink}>Explore Event</Link> */}
+          <Button link={exploreLink}>
+            <span>
+            Explore Event
+            </span>
+            <span className={classes.icon} >
+              <ArrowRightIcon/>
+            </span>
+          </Button>
         </div>
       </div>
     </li>
