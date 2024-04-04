41b2b18ebf5898530fe5cab98574c5718df64d08
Prashant kumhar <prkumar@griddynamics.com>
2024-04-04 11:05:45 +0530

    event detail is added

--- nextjs-course-code-03-prj-routing-starting-setup/pages/events/[eventId].jsx
+++ nextjs-course-code-03-prj-routing-starting-setup/pages/events/[eventId].jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
 
 const EventDetailPage = () => {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>No Event Found!</p>
+  }
   return (
-    <div>EventDetailPage</div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics date={event.date} address={event.location} image={event.image} imgAlt={event.title} />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   )
 }
 
